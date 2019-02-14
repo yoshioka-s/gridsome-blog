64446d78c2e11e58e81c1ac193962343464d2ec1
Shu Yoshioka <yoshioka63@gmail.com>
2019-02-14 17:39:00 +0900

    Init gridsome

---
+++ gridsome.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: [
+    {
+      use: '@gridsome/source-wordpress',
+      options: {
+        baseUrl: 'YOUR_WEBSITE_URL', // required
+        typeName: 'WordPress', // GraphQL schema name (Optional)
+        perPage: 100, // How many posts to load from server per request (Optional)
+        concurrent: 10, // How many requests to run simultaneously (Optional)
+        routes: {
+          post: '/:year/:month/:day/:slug', //adds route for "post" post type (Optional)
+          post_tag: '/tag/:slug' // adds route for "post_tag" post type (Optional)
+        }
+      }
+    }
+  ]
+}
